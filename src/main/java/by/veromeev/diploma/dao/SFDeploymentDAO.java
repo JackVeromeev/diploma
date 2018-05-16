e1df4922bf0379f727ba1c402991e4023a39f3f3
Yakov Veromeev <yakov.veromeev@vrpconsulting.com>
2018-05-16 19:48:13 +0300

    remove Buttons and Deployments

--- src/main/java/by/veromeev/diploma/dao/SFDeploymentDAO.java
+++
@@ -1,7 +0,0 @@
-package by.veromeev.diploma.dao;
-
-import by.veromeev.diploma.entity.SFDeployment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SFDeploymentDAO extends CrudRepository<SFDeployment, Long> {
-}
--- src/main/java/by/veromeev/diploma/entity/ChatWindow.java
+++ src/main/java/by/veromeev/diploma/entity/ChatWindow.java
@@ -3,10 +3,11 @@ package by.veromeev.diploma.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
 @Entity
@@ -17,12 +18,19 @@ public class ChatWindow extends AbstractEntity {
 
     private static final long serialVersionUID = 14485663450357523L;
 
-    @ManyToOne
-    private SFDeployment deployment;
-
-    @ManyToOne
-    private SFButtons buttons;
-
     @OneToMany()
     private Set<ChatSession> chatSessions;
+
+    @NotBlank
+    @Length(min = 15, max = 18)
+    private String SFOrganizationId;
+
+    @NotBlank
+    @Length(min = 15, max = 18)
+    private String SFButtonsId;
+
+    @NotBlank
+    @Length(min = 15, max = 18)
+    private String SFDeploymentId;
+
 }
